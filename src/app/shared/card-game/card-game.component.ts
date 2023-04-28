53bceaf389c7965494f7ab3d54b5578d519a4893
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-28 17:19:17 -0300

    adicionado as imagem na section destaque

--- src/app/shared/card-game/card-game.component.ts
+++ src/app/shared/card-game/card-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-game',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-game.component.css']
 })
 export class CardGameComponent implements OnInit {
-
+  @Input() backGroundImage = '';
   constructor() { }
 
   ngOnInit(): void {
