5c5c509c108e03ecb6e207825d526b126fc33858
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-29 18:56:08 -0300

    melhorias no estilos dos menus

--- src/app/components/header-playstore/header-playstore.component.ts
+++ src/app/components/header-playstore/header-playstore.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header-playstore',
   templateUrl: './header-playstore.component.html',
   styleUrls: ['./header-playstore.component.css']
 })
-export class HeaderPlaystoreComponent implements OnInit {
+export class HeaderPlaystoreComponent {
+  top = '0px';
+  prevScrollPos = window.pageYOffset;
 
-  constructor() { }
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const currentScrollPos = window.pageYOffset;
+    if (this.prevScrollPos > currentScrollPos) {
 
-  ngOnInit(): void {
+      this.top = '40px'
+    } else {
+      this.top = '0px';
+    }
+    this.prevScrollPos = currentScrollPos;
   }
 
 }
--- src/app/components/header-sony/header-sony.component.ts
+++ src/app/components/header-sony/header-sony.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header-sony',
   templateUrl: './header-sony.component.html',
   styleUrls: ['./header-sony.component.css']
 })
-export class HeaderSonyComponent implements OnInit {
+export class HeaderSonyComponent {
+  isFixed = false;
+  prevScrollPos = window.pageYOffset;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const currentScrollPos = window.pageYOffset;
+    if (this.prevScrollPos > currentScrollPos) {
+      this.isFixed = true;
+    } else {
+      this.isFixed = false;
+    }
+    this.prevScrollPos = currentScrollPos;
   }
 
 }
