dfc46e9e22cc323dac40437cf6443693f5f5b9dd
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-29 23:15:36 -0300

    correções de estilo no section about

--- src/app/components/section-more-about/section-more-about.component.ts
+++ src/app/components/section-more-about/section-more-about.component.ts
@@ -12,6 +12,8 @@ export class SectionMoreAboutComponent {
     "https://image.api.playstation.com/pr/bam-art/109/464/81c5c029-5ef2-4e37-b5a0-3426be21175f.png?w=440&thumb=false",
     "https://image.api.playstation.com/pr/bam-art/110/565/aedbe663-b3d6-4c6f-b43c-4ae83cbd8c10.jpg?w=440&thumb=false",
     "https://image.api.playstation.com/pr/bam-art/136/935/fe5b77a8-16ee-4647-b676-bcb1a28cac2a.png?w=440&thumb=false",
-    "https://image.api.playstation.com/pr/bam-art/111/152/2a125dbb-091c-4a5b-a7e3-1cf483daf439.jpg?w=440&thumb=false"
+    "https://image.api.playstation.com/pr/bam-art/111/152/2a125dbb-091c-4a5b-a7e3-1cf483daf439.jpg?w=440&thumb=false",
+    'https://image.api.playstation.com/pr/bam-art/108/737/a296b1f1-3dee-448f-aeab-7e31dec98048.png?w=440&thumb=false',
+    'https://image.api.playstation.com/pr/bam-art/156/127/64fdc096-e507-406c-a6bc-db88a6081ee2.png?w=440&thumb=false'
   ];
 }
