6f31fe0708833f11f367e6328573172f3f816e7d
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-05-02 11:42:53 -0300

    adcionado novas informações nos jogos

--- src/app/db/games.ts
+++ src/app/db/games.ts
@@ -1,115 +1,230 @@
 export const games = [
   {
+    id: 0,
+    title: 'EA SPORTS™ FIFA 23',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202301/0312/yM0eeJui8AFByeP5BC5XV5j9.png?w=230&thumb=false",
-    tags: ["bestTem"],
+    tags: ["bestTem", 'PS5',
+      'EDIÇÃO STANDARD'],
+    createdBy: 'Electronic Arts Inc',
+    price: 333.90
   },
   {
+    id: 1,
+    title: 'STAR WARS Jedi: Survivor™',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202211/2222/l8QTN7ThQK3lRBHhB3nX1s7h.png?w=230&thumb=false",
-    tags: ["bestTem", "new"],
+    tags: ["bestTem", "new", 'PS5',
+      'EDIÇÃO STANDARD'],
+    price: 338.90
   },
   {
+    id: 2,
+    title: 'Fortnite',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202303/0621/d3c11818a78c6495e84a3d8e8dd6dc652721be36e0eb8c0a.png?w=230&thumb=false",
-    tags: ["bestTem"],
+    tags: ["bestTem", "free"],
+    createdBy: "EPIC GAMES",
+    price: 3,
   },
   {
+    id: 4,
+    title: 'Call of Duty®: Modern Warfare® II - Pacote Multigeração',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202304/1300/69e8f4d30c2b6544938b4a3754f3089445c74527c0b821d6.png?w=230&thumb=false",
-    tags: ["bestTem"],
+    tags: ["bestTem", 'PS4', 'PS5', 'CROSS-GEN BUNDLE'],
+    createdBy: "Activision",
+    price: 329.00,
   },
   {
+    id: 5,
+    title: 'The Last of Us™ Parte II',
     image:
       "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/w48z6bzefZPrRcJHc7L8SO66.png?w=230&thumb=false",
     tags: ["bestTem"],
   },
   {
+    id: 6,
+    title: 'Grand Theft Auto V (PlayStation®5)',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202202/2816/K6mmm89oNII1iI1aqaClO0wh.png?w=230&thumb=false",
-    tags: ["bestTem"],
+    tags: ["bestTem", 'PS5'],
+    createdBy: "Rockstar Games",
+    price: 107.90,
   },
   {
+    id: 7,
+    title: 'Hogwarts Legacy',
+    image:
+      "https://image.api.playstation.com/vulcan/ap/rnd/202011/0919/JmxLZt6exeqcKRz7BSmK8aId.png?w=230&thumb=false",
+    tags: ["bestTem", 'PS5', 'EDIÇÃO STANDARD'],
+    createdBy: "Warner Bros. Interactive",
+    price: 299.90,
+  },
+  {
+    id: 8,
+    title: 'Resident Evil 4',
+    image:
+      "https://image.api.playstation.com/vulcan/ap/rnd/202207/2509/85p2Dwh5iDhUzRKe40QeNYh3.png?w=230&thumb=false",
+    tags: ["bestTem", 'PS4', 'PS5', 'EDIÇÃO STANDARD'],
+    createdBy: "Capcom U.S.A., Inc.",
+    price: 259.90,
+  },
+  {
+    id: 9,
+    title: 'NBA 2K23',
+    image:
+      "https://image.api.playstation.com/vulcan/ap/rnd/202208/3122/LPx7mwV5n0YAczzjVIjfP3tt.png?w=230&thumb=false",
+    tags: ["bestTem", 'PS5', 'EDIÇÃO STANDARD'],
+    createdBy: "2K",
+    price: 52.90,
+  },
+  {
+    id: 10,
+    title: "Assassin's Creed® Valhalla",
+    image:
+      "https://image.api.playstation.com/vulcan/ap/rnd/202006/1520/EDtkdijFRwbmGKk1G9lrDoEF.png?w=230&thumb=false",
+    tags: ["bestTem", 'PS4', 'PS5'],
+    createdBy: "Ubisoft Entertainment",
+    price: 69.90,
+  },
+  {
+    id: 11,
+    title: 'BROK the InvestiGator DEMO',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202303/1823/5ee4f1cca98c641f68ed17fe19388791d03a3ce170575a76.png?w=230&thumb=false",
-    tags: ["demo"],
+    tags: ["demo", "free", 'PS4'],
+    createdBy: "Fabrice Breton trading as COWCAT",
+    price: 0,
   },
   {
+    id: 12,
+    title: 'BROK the InvestiGator DEMO',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202303/1823/5ee4f1cca98c641f68ed17fe19388791d03a3ce170575a76.png?w=230&thumb=false",
-    tags: ["demo"],
+    tags: ["demo", "free", 'PS4'],
+    createdBy: "Fabrice Breton trading as COWCAT",
+    price: 0,
   },
   {
+    id: 13,
+    title: 'Street Fighter™ 6 Demo',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202304/0304/b46d9adfafd011a50593b2ebd499610d087fb225f79a5b40.png?w=230&thumb=false",
-    tags: ["demo"],
+    tags: ["demo", "free"],
+    createdBy: "Capcom U.S.A., Inc.",
+    price: 0,
   },
   {
+    id: 14,
+    title: 'Street Fighter™ 6 Demo',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202304/0304/b46d9adfafd011a50593b2ebd499610d087fb225f79a5b40.png?w=230&thumb=false",
-    tags: ["demo"],
+    tags: ["demo", "free"],
+    createdBy: "Capcom U.S.A., Inc.",
+    price: 0,
   },
   {
+    id: 15,
+    title: 'THE KING OF FIGHTERS XV DEMO',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202303/0609/5a9d59570fae517969d6e36568efe794ddc8d47243dbeb32.png?w=230&thumb=false",
-    tags: ["demo"],
+    tags: ["demo", "free"],
   },
   {
+    id: 16,
+    title: 'I am an Air Traffic Controller AIRPORT HERO HANEDA',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202304/0308/9f45ecff21d8d1647f5a6972da6ee6a72493031247900731.png?w=230&thumb=false",
-    tags: ["demo"],
+    tags: ["demo", 'free'],
+    createdBy: "SONIC POWERED CO.,LTD.",
+    price: 0,
   },
   {
+    id: 17,
+    title: 'Dead Island 2',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202208/1012/V6WJkRhfNIQ3ePfAmrLSfGng.png?w=230&thumb=false",
     tags: ["new"],
+    createdBy: "Deep Silver",
+    price: 390.00,
   },
   {
+    id: 18,
+    title: 'Minecraft Legends',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202207/1516/C4e7doLUZkF9fDviGRHO1n2x.png?w=230&thumb=false",
-    tags: ["new"],
+    tags: ["new", 'PS4', 'PS5'],
+
+    createdBy: "Mojang",
+    price: 199.00,
   },
   {
+    id: 19,
+    title: 'FINAL FANTASY VI',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202212/0511/3tqbQ0Mjd0JwmcfGEGv6c4i6.png?w=230&thumb=false",
-    tags: ["new"],
+    tags: ["new", 'PS4', 'FINAL FANTASY VI'],
+
+    createdBy: "SQUARE ENIX CO. LTD.",
+    price: 95.00,
   },
   {
+    id: 20,
+    title: 'Disney Speedstorm - Pacote de Criador Padrão',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202304/1820/089786a9937a0074b674f3494b0bf710c68e16b4a11f47fc.png?w=230&thumb=false",
-    tags: ["new"],
+    tags: ["new", 'PS4', 'PACOTE DE CRIADOR PADRÃO'],
+    createdBy: "SQUARE ENIX CO. LTD.",
+    price: 159.00,
   },
   {
+    id: 21,
+    title: 'Afterimage PS4 & PS5',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202304/1119/195cdeb4a42e21b335183060d5dfe1b49f4fc92c87429f92.png?w=230&thumb=false",
-    tags: ["new"],
+    tags: ["new", 'PS4'],
+    createdBy: "Modus Games LLC",
+    price: 124.00,
   },
   {
+    id: 22,
+    title: '',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202211/3017/Oo1B84A7BLCT157YFSxjtwG0.png?w=230&thumb=false",
     tags: ["shortly"],
   },
   {
+    id: 23,
+    title: '',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202211/1407/XFU0aPBvtm3W2od1ZvhByAOv.png?w=230&thumb=false",
     tags: ["shortly"],
   },
   {
+    id: 24,
+    title: '',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202211/3007/lgFVhRm5BfoX02pRUt3lSmLV.png?w=230&thumb=false",
     tags: ["shortly"],
   },
   {
+    id: 25,
+    title: '',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202212/1301/4f6q3hFlVbI3FSbDDpH9I4uA.png?w=230&thumb=false",
     tags: ["shortly"],
   },
   {
+    id: 26,
+    title: '',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202212/0214/CfyekgsrgeaZlAfgVDeK10ZF.png?w=230&thumb=false",
     tags: ["shortly"],
   },
   {
+    id: 27,
+    title: '',
     image:
       "https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/ba706e54d68d10a0eb6ab7c36cdad9178c58b7fb7bb03d28.png?w=230&thumb=false",
     tags: ["shortly"],
